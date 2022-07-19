7bb58bfe6c8a8875e99f2b69b06d89c714f6fd3d
Arpit Patel <73097367+Arpit2298@users.noreply.github.com>
2022-07-19 18:33:23 +0530

    KP_Final

---
+++ admin/admin_assets/js/custom/alert-redirection.js
@@ -0,0 +1,16 @@
+﻿function alertPageTransfor(pagename) {
+    if (document.getElementById("content_page_hfRedirect").value == "1") {
+        $('.confirm').click(function () {
+            debugger;
+            window.location.href = pagename;
+        })
+    }
+}
+
+function alertPageTransforFormPage(pagename) {
+    if (document.getElementById("content_page_secound_content_hfRedirect").value == "1") {
+        $('.confirm').click(function () {
+            window.location.href = pagename;
+        })
+    }
+}
